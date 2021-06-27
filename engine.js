9c76cd47246e48ac92f1cde3a06b9d428b263183
Molnár István <33826380+molnaristvan95@users.noreply.github.com>
2021-06-27 23:51:34 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,10 @@
-window.alert('mux');
+console.clear();
+console.time();
+console.group();
+
+
+
+
+
+console.timeEnd();
+console.log('%c The program successfully was loaded', 'font-size:25px; color:#ff0;');
